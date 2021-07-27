c5dd0cf77ab8feffd3ac9a08ccf0f344b9732914
Githb-usr <tragopan@free.fr>
2021-07-27 16:02:09 +0200

    correctif erreurs persos

--- food/views.py
+++ food/views.py
@@ -209,13 +209,13 @@ def determine_nutriment_level_data(product):
     return nutriment_level_data
 
 def handler404(request, exception, template_name="404.html"):
-    response = render_to_response('404.html')
+    response = render(request, '404.html')
     response.status_code = 404
 
     return response
 
 def handler500(request, exception, template_name="500.html"):
-    response = render_to_response('500.html')
+    response = render(request, '500.html')
     response.status_code = 500
 
     return response
