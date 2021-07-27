4af5ba506b85be8e04f01cfcc7901a24efa074da
Githb-usr <tragopan@free.fr>
2021-07-27 16:11:58 +0200

    correctif 4 erreurs persos

--- food/views.py
+++ food/views.py
@@ -4,7 +4,6 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
-from django.template import RequestContext
 from django.views.generic import TemplateView, ListView
 import logging
 
